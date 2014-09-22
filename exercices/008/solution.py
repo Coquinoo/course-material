c79ecbfccbcd1862b32df530cb98af52a886ed68
Alexandre Vaugoux <alexandre.vaugoux@gmail.com>
2014-09-22 15:17:04 +0200

    solut008

---
+++ exercices/008/solution.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Éditeur de Spyder
+
+Ceci est un script temporaire.
+"""
+list = ["H", "e", "l", "l", "o", " ", "w", "o", "r", "l", "d", " ", "!"]
+for i in list:
+    print(i)
