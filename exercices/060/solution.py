e0736f70ed81c14f3f122867ac6f915dee7c3eae
Alexandre Vaugoux <alexandre.vaugoux@gmail.com>
2014-09-24 18:15:25 +0200

    solution 060

---
+++ exercices/060/solution.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Éditeur de Spyder
+
+Ceci est un script temporaire.
+"""
+alphabets = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+keywords = []
+for l1 in alphabets:
+    for l2 in alphabets:
+            keywords.append(l1+l2)
+            
+print '\n'.join(keywords)
